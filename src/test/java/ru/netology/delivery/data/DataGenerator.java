91018de648793feef2a24c46ad545145870e7d5c
Ivan Vasilev <tor419@yandex.ru>
2023-04-20 15:08:58 +0700

    chore: add tests three commit

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -16,29 +16,21 @@ public class DataGenerator {
 
     public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
     }
 
     public static String generateCity(String locale) {
         var cities = new String[]{"Новосибирск", "Уфа", "Улан-удэ", "Элиста", "Симферополь", "Йошкар-Ола", "Москва", "Санкт-Петербург"};
         return cities[new Random().nextInt(cities.length)];
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
     }
 
     public static String generateName(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.name().lastName() + " " + faker.name().firstName();
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
     }
 
     public static String generatePhone(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
     }
 
     public static class Registration {
@@ -47,9 +39,6 @@ public class DataGenerator {
 
         public static UserInfo generateUser(String locale) {
             return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-
         }
     }
 
